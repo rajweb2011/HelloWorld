0fa701774e5107928309129a41920dba2a4c77ea
rajweb2011 <36100521+rajweb2011@users.noreply.github.com>
2018-03-13 13:11:13 +0000

    Add files via upload

---
+++ HelloWorld/src/main/java/HelloWorld/HelloWorld/App.java
@@ -0,0 +1,17 @@
+package HelloWorld.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for(int  i=1;i<=10;i++){
+    		System.out.println( "Hello World! " );
+    		System.out.println( "Rajesh your ready for jenkins jobs test  ... " );
+    	}
+    	
+    }
+}
