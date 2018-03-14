a88b46394f0f1abe25bcb0aaa5b1e49f54a448bd
rajweb2011 <36100521+rajweb2011@users.noreply.github.com>
2018-03-14 16:37:46 +0000

    Update App.java

--- HelloWorld/src/main/java/HelloWorld/HelloWorld/App.java
+++ HelloWorld/src/main/java/HelloWorld/HelloWorld/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	for(int  i=1;i<=10;i++){
-    		System.out.println( "Hello World!welcome bhanu  " );
+    		System.out.println( "Hello World!welcome sindhura  " );
     		System.out.println( "Rajesh your ready for jenkins jobs test  ... " );
     	}
     	
